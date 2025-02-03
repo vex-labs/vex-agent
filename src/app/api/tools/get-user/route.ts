1511fd696f72d38169e0d01c9dd5b81e180a67dd
Ruben Marcus <ruben@rubenmarcus.dev>
2025-02-03 13:41:36 +0000

    fix next.js 15

--- src/app/api/tools/get-user/route.ts
+++ src/app/api/tools/get-user/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
 
-  const headersList = headers();
+  const headersList = await headers();
   const mbMetadata = JSON.parse(headersList.get('mb-metadata') || '{}');
   const accountId = mbMetadata?.accountData?.accountId || 'near';
 
