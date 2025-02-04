e6d7ffe3065187bf52aed35a141461e3640958ba
Sérgio <sergioseco98@gmail.com>
2025-02-04 18:36:48 +0000

    CORS + Detect dev url

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites(){
-        return [
-            {
-                source: "/.well-known/ai-plugin.json",
-                destination: "/api/ai-plugin"
-            }
-        ]
-    }
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/ai-plugin.json",
+        destination: "/api/ai-plugin",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/config.ts
+++ src/app/config.ts
@@ -1,13 +1,9 @@
 import { DEPLOYMENT_URL } from "vercel-url";
 
 const ACCOUNT_ID = process.env.ACCOUNT_ID;
-const BITTE_CONFIG = JSON.parse(process.env.BITTE_CONFIG || "{}");
-
-// Plugin url set in BITTE_CONFIG by make-agent during development
-const debugPluginUrl = BITTE_CONFIG.url;
 
 // Set the plugin url in order of BITTE_CONFIG, env, DEPLOYMENT_URL (used for Vercel deployments)
-const PLUGIN_URL = debugPluginUrl || DEPLOYMENT_URL;
+const PLUGIN_URL = DEPLOYMENT_URL || `${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.PORT || 3000}`;
 
 if (!PLUGIN_URL) {
   console.error(
